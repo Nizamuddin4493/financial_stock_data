a63dc55816dc67a1ff67b46943146ada94588f4d
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-06 21:38:05 +0430

    category component styled is complete

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Categories from './Categories';
 import Header from './Header';
 import SearchForm from './SearchForm';
 
@@ -9,6 +10,7 @@ function HomePage() {
         <Header />
         <hr />
         <SearchForm />
+        <Categories />
       </div>
     </>
   )
