00b5c376d6e47b40461be65a8d38b74c08286a01
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-06 15:09:48 +0430

    creating components

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/homePageDesign.css';
+
+function HomePage() {
+  return (
+    <>
+      <div className="homePageDesign">
+        fgdfgdfgfdgfdg
+      </div>
+    </>
+  )
+}
+
+export default HomePage;
