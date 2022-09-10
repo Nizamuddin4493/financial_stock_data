63476dfdae8c38d79bbd65beedeb9cbfb4ee9488
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-10 19:58:09 +0430

    updating the title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
   <>
     <div className="header">
-      <h1>Financial Stock Data</h1>
+      <h1>Financial Stock and Coins Data</h1>
     </div>
   </>
 );
