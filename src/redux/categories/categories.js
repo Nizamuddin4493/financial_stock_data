4b6d8c1c6cac0fca1dc9d5af53530c976900c2a6
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-10 20:13:21 +0430

    removing conflict data.

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,15 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-<<<<<<< HEAD
 
 const FETCH_DATA = './financial_Stock/data/FETCH_DATA';
 const INITIAL_DATA = [];
 
-=======
-
-const FETCH_DATA = './financial_Stock/data/FETCH_DATA';
-const INITIAL_DATA = ['testing'];
-
->>>>>>> ed2917d78aa2a66841f2380c05bdf3df05428cb6
 export const CategoriesAction = (payload) => (
   {
     type: FETCH_DATA,
@@ -17,25 +10,6 @@ export const CategoriesAction = (payload) => (
   }
 );
 
-<<<<<<< HEAD
-=======
-// export const categoriesThunk = createAsyncThunk(
-//   FETCH_DATA,
-//   async () => {
-//     const res = await fetch('https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=08b1d4086627a53d104fcffc0a8db180');
-//     const categories = await res.json();
-//     const fetchCategoriesData = categories.map((data) => ({
-//       symbol: data.symbol,
-//       name: data.name,
-//       change: data.change,
-//       price: data.price,
-//       changesPercentage: data.changesPercentage,
-//     }));
-//     return fetchCategoriesData;
-//   },
-// );
-
->>>>>>> ed2917d78aa2a66841f2380c05bdf3df05428cb6
 export const categoriesThunk = createAsyncThunk(
   FETCH_DATA,
   async () => {
@@ -46,7 +20,6 @@ export const categoriesThunk = createAsyncThunk(
       symbol: data.symbol,
       name: data.name,
       image: data.image,
-<<<<<<< HEAD
       currentPrice: data.current_price,
       totalVolume: data.total_volume,
       high24h: data.high_24h,
@@ -56,9 +29,6 @@ export const categoriesThunk = createAsyncThunk(
       totalSupply: data.total_supply,
       atlChangePercentage: data.atl_change_percentage,
       lastUpdated: data.last_updated,
-=======
-      total_volume: data.total_volume,
->>>>>>> ed2917d78aa2a66841f2380c05bdf3df05428cb6
     }));
     return fetchCategoriesData;
   },
@@ -67,11 +37,7 @@ export const categoriesThunk = createAsyncThunk(
 export default function categoriesReducer(state = INITIAL_DATA, action) {
   switch (action.type) {
     case `${FETCH_DATA}/fulfilled`:
-<<<<<<< HEAD
       return action.payload;
-=======
-      return [...action.payload];
->>>>>>> ed2917d78aa2a66841f2380c05bdf3df05428cb6
     default:
       return state;
   }
